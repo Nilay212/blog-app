8fc350eb6e518215e3fd555242427a427a690a96
Nilay <pandaynilay@gmail.com>
2024-04-28 22:24:46 +0530

    Header section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
+
 
 function App() {
   return (
+    <>
+    <Header/>
     <Routes>
       <Route path="/" element= {<Home />} />
       <Route path="/about" element= {<About />} />
@@ -16,6 +20,7 @@ function App() {
       <Route path="/dashboard" element= {<Dashboard />} />
       <Route path="/projects" element= {<Projects />} />
     </Routes>
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "../src/index.css"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 )

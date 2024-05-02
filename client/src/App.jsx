e86664e2432010747071e725fae72cb13027cd52
Nilay <pandaynilay@gmail.com>
2024-05-02 18:01:32 +0530

    corrected css not showing error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,12 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 
 function App() {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path="/" element= {<Home />} />
       <Route path="/about" element= {<About />} />
@@ -16,6 +19,7 @@ function App() {
       <Route path="/dashboard" element= {<Dashboard />} />
       <Route path="/projects" element= {<Projects />} />
     </Routes>
+    </>
   )
 }
 

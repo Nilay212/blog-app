f76249865376550fc07b4658629fca05c1f5cedc
Nilay <pandaynilay@gmail.com>
2024-05-04 20:41:25 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt"
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 
 export const signup = async (req,res) => {
     try {
@@ -33,5 +34,31 @@ export const signup = async (req,res) => {
     } catch (error) {
         console.log("Error in signup controller", error.message);
         res.status(500).json({error : "Internal server Error"});
+    }
+}
+
+export const signin = async (req,res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({error : "All fields are required"});
+        }
+
+        const user = await User.findOne({email});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        if(!user || !isPasswordCorrect) {
+            return res.status(404).json({error : "Invalid Credentials"})
+        }
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id : user._id,
+            username : user.username,
+        })
+        
+    } catch (error) {
+        console.log("Error in signin controller : ", error.message);
+        res.status(500).json({error : "Internal Server Error"});
     }
 }

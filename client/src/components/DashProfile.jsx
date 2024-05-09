3414827836e63e86e2fa2367d6c9ba8088907fad
Nilay <pandaynilay@gmail.com>
2024-05-09 23:20:05 +0530

    completed post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,9 +21,10 @@ import {
 } from "../redux/user/userSlice.js"
 import { useDispatch } from "react-redux"
 import {HiOutlineExclamationCircle} from "react-icons/hi"
+import {Link} from 'react-router-dom'
 
 function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user)
+  const { currentUser, error, loading } = useSelector((state) => state.user)
   const [imageFile, setImageFile] = useState(null)
   const [imageFileUrl, setImageFileUrl] = useState(null)
   const filePickerRef = useRef()
@@ -235,9 +236,18 @@ function DashProfile() {
           placeholder='password'
           onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading} >
+          {loading ? 'Loading...' : "Update"}
         </Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'} >
+            <Button type="button" gradientDuoTone='purpleToPink' className="w-full"  >
+              Create a post
+            </Button>
+            </Link>
+          )
+        }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer' onClick={() => setShowModal(true)}>Delete Account</span>
